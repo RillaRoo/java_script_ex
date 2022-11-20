2df9bc74215347c21711765dee58fb00a236465d
RillaRoo <63817616+RillaRoo@users.noreply.github.com>
2022-11-20 16:16:35 +0000

    Add files via upload

---
+++ exercicio_javascript/script.js
@@ -0,0 +1,32 @@
+let pessoa = 
+{
+    nome : "Bruno",
+    idade : 24,
+};
+
+console.log(pessoa.nome,pessoa.idade);
+    
+let idade2 = 7*pessoa.idade;
+    
+console.log(idade2)
+
+let amigos = ["Carlos", "Joana", "Rita"]
+
+let estudantes = [
+{
+    nome: "Carlos",
+    idade: 22,
+    isStudent: true    
+},
+{
+    nome: "Joana",
+    idade: 20,
+    isStudent: true
+},
+{
+    nome: "Carlos",
+    idade: 32   ,
+    isStudent: false
+}] 
+
+console.log(nome.estudante,idade.estudante,isStudent.estudante);
